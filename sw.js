1299c60aa202de98a29ed7811bc0e781f59b10ec
Anna Rufova <kuetama@gmail.com>
2025-12-16 14:31:05 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,24 @@
-// sw.js
-self.addEventListener('install', () => {
-  self.skipWaiting();
+const CACHE_NAME = 'memory-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+ '/css/style.css',
+  '/js/main.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
